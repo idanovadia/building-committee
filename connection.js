0bd4fee0a0f214b55429da584e6ac4ddfa504312
idan <idanov@post.bgu.ac.il>
2020-08-01 23:24:49 +0300

    connect to azure mysql

--- connection.js
+++ connection.js
@@ -2,13 +2,23 @@
 const mysql = require("mysql");
 
 var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
+    host: 'idan.mysql.database.azure.com',
     port : '3306',
-    user : 'root',
-    password : 'idaN1234!',
+    user : 'idanov@idan',
+    password : 'idaN1234',
     database : 'committee',
+    ssl: true,
     multipleStatements : true
 });
+
+// var mysqlConnection = mysql.createConnection({
+//   host: 'localhost',
+//   port : '3306',
+//   user : 'root',
+//   password : 'idaN1234!',
+//   database : 'committee',
+//   multipleStatements : true
+// });
 
 mysqlConnection.connect((err)=> {
     if (err) {
